083299bc0a00a2827afc40dc89247b8acdab186a
sam <samuel.escobar.restrepo@correounivalle.edu.co>
2025-10-02 03:44:15 -0500

    clean

--- controls.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("keydown", function(event) {
-    const output = document.getElementById("output");
-    output.textContent = `Tecla presionada: ${event.key} | Código: ${event.code}`;
-});
